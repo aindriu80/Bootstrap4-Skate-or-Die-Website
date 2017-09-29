cb90ee3a0aca88e82205501d93ce64ccb8c4bd9a
aindriu80 <aindriu80@gmail.com>
2017-09-29 10:16:41 +0100

    JavaScript Register Form Validation Part 2

--- inc/footer.php
+++ inc/footer.php
@@ -5,4 +5,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
 
 
+
 <script src="/resources/js/phpjavascript.js"></script>
--- resources/js/phpjavascript.js
+++ resources/js/phpjavascript.js
@@ -4,14 +4,33 @@ $(document)
 	event.preventDefault();
 
 	var _form = $(this);
+    var _error = $(".js-error", _form);
 	
 
 	var data = {
 		email: $("input[type='email']", _form).val(),
 		password: $("input[type='password']", _form).val()
 	};
-    console.log(data);
-	alert('form was submitted');
+    
+    if(data.email.length < 6) {
+		_error
+			.text("Please enter a valid email address")
+			.show();
+		return false;
+	} else if (data.password.length < 11) {
+		_error
+			.text("Please enter a passphrase that is at least 11 characters long.")
+			.show();
+		return false;
+	}
+    
+    // Assuming the code gets this far, we can start the ajax process
+	_error.hide();
 
 	return false;
+    
+//    console.log(data);
+//	alert('form was submitted');
+
+//	return false;
 })
